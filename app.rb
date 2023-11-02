d9d3e027fab57920748124055fe333476fcdb0ff
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-02 14:24:04 +0000

    added if statement

--- app.rb
+++ app.rb
@@ -7,3 +7,30 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+ moves = ['rock', 'paper', 'scissors']
+ 
+ comp_moves = moves.sample
+  
+ if comp_moves == "rock"
+    outcome = "tied"
+ elsif comp_moves == "paper"
+    outcome = "lost"
+ else comp_moves == "scissors"
+    outcome = "won"
+ end
+ "
+  <h2>
+    We played rock!
+  </h2>
+
+  <h2>
+   They played #{comp_moves}!
+  </h2>
+
+  <h2>
+    We #{outcome}!
+  </h2>
+  "
+end

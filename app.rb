2d5c390474c975549adb8f4db21d2c0630e6ad85
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-11-02 15:00:43 +0000

    created zebra.erb

--- app.rb
+++ app.rb
@@ -11,26 +11,16 @@ end
 get("/rock") do
  moves = ['rock', 'paper', 'scissors']
  
- comp_moves = moves.sample
+ @comp_moves = moves.sample
   
- if comp_moves == "rock"
-    outcome = "tied"
- elsif comp_moves == "paper"
-    outcome = "lost"
- else comp_moves == "scissors"
-    outcome = "won"
+ if @comp_moves == "rock"
+    @outcome = "tied"
+ elsif @comp_moves == "paper"
+    @outcome = "lost"
+ else 
+    @outcome = "won"
  end
- "
-  <h2>
-    We played rock!
-  </h2>
-
-  <h2>
-   They played #{comp_moves}!
-  </h2>
-
-  <h2>
-    We #{outcome}!
-  </h2>
-  "
+ 
+ erb(:zebra)
+  
 end
